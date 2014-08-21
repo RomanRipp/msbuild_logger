bafc37003b03287de4f530d69dc988f03253b0c5
unknown <romanr@ROMANR-DPM4800.dpcorp.int>
2014-08-21 14:49:34 -0700

    Initial commit

---
+++ Constants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EspritLogger
+{
+    class Constants
+    {
+        //delta calculation tolerance
+        public static int DELTA_TOLERANCE = 0;
+        //excel file cell formating
+        public static string COLOR_RED = "#FF6666";
+        public static string COLOR_YELLOW = "#FFFF99";
+        public static string COLOR_GREEN = "#CCFF99";
+          
+        //excel file column formating
+        public static int DATE_COLUMN_WIDTH = 16;
+        public static int PROJECTS_COLUMN_WIDTH = 12;
+    }
+}
